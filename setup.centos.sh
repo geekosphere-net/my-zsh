6f1753acb54be89233e01a67cc32c8ca5d994ac5
Devin Richards <devin@geekosphere.net>
2017-05-16 09:57:10 -0400

    disabled selinux and firewalld

--- setup.centos.sh
+++ setup.centos.sh
@@ -1,11 +1,33 @@
 #!/bin/bash
 
-yum install -y epel-release
-yum install -y git wget yum-utils net-tools kernel-headers kernel-devel gcc zsh vim figlet
+# setup repos
+sudo yum install -y epel-release
+sudo yum install -y git wget yum-utils net-tools kernel-headers kernel-devel gcc zsh vim figlet
 
+# oh-my-zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
-
 git clone https://github.com/zsh-users/zsh-completions ~/.oh-my-zsh/custom/plugins/zsh-completions
 
+# git-flow-avh
 wget --no-check-certificate -q  https://raw.githubusercontent.com/petervanderdoes/gitflow-avh/develop/contrib/gitflow-installer.sh && sudo bash gitflow-installer.sh install stable; rm gitflow-installer.sh
+
+# disable firewall
+sudo systemctl disable firewalld
+sudo systemctl stop firewalld
+sudo systemctl disable iptables
+sudo systemctl stop iptables
+
+# disable selinux
+echo "SELINUX=disabled\nSELINUXTYPE=targeted\nSETLOCALDEFS=0" >> /tmp/selinuxconfig
+[[ -f /etc/selinux/config ]] && sudo cp /etc/selinux/config /etc/selinux/config.orig
+sudo mv /tmp/selinuxconfig /etc/selinux/config
+
+# install vmware tools
+sudo mkdir /tmp/a
+sudo mount /dev/cdrom /tmp/a
+sudo ln -s /mnt/hgfs/c /mnt/c
+sudo ln -s /mnt/hgfs/d /mnt/d
+figlet "install VMware Tools"
+
+
 
--- setup.ubuntu.sh
+++ setup.ubuntu.sh
@@ -1,18 +1,32 @@
 #!/bin/bash
 
-# copy sshkey
-
 # setup basics
-apt install -y wget curl net-tools zsh vim git git-flow apt-transport-https ca-certificates software-properties-common sysvbanner figlet
+sudo apt install -y wget curl net-tools zsh vim git git-flow apt-transport-https ca-certificates software-properties-common sysvbanner figlet
 
 # setup OhMyZsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 git clone https://github.com/zsh-users/zsh-completions ~/.oh-my-zsh/custom/plugins/zsh-completions
-echo vim ~/.zshrc
+
+# git-flow-avh
+wget --no-check-certificate -q  https://raw.githubusercontent.com/petervanderdoes/gitflow-avh/develop/contrib/gitflow-installer.sh && sudo bash gitflow-installer.sh install stable; rm gitflow-installer.sh
+
+# disable firewall
+sudo systemctl disable firewalld
+sudo systemctl stop firewalld
+sudo systemctl disable iptables
+sudo systemctl stop iptables
+
+# disable selinux
+echo "SELINUX=disabled\nSELINUXTYPE=targeted\nSETLOCALDEFS=0" >> /tmp/selinuxconfig
+[[ -f /etc/selinux/config ]] && sudo cp /etc/selinux/config /etc/selinux/config.orig
+sudo mv /tmp/selinuxconfig /etc/selinux/config
 
 # install vmware tools
-mount /dev/cdrom /tmp/a
-ln -s /mnt/hgfs/c /mnt/c
-ln -s /mnt/hgfs/d /mnt/d
+sudo mkdir /tmp/a
+sudo mount /dev/cdrom /tmp/a
+sudo ln -s /mnt/hgfs/c /mnt/c
+sudo ln -s /mnt/hgfs/d /mnt/d
+figlet "install VMware Tools"
+
 
 
