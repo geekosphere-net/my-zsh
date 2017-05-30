bbf592d9c5098fb861551aef0dd677d13f26b0a7
Devin Richards <drichards@leveragingtechnology.com>
2017-05-30 15:33:51 -0400

    moved aliases to devin.zsh

--- .oh-my-zsh/custom/devin.zsh
+++ .oh-my-zsh/custom/devin.zsh
@@ -5,3 +5,9 @@ alias mark="figlet -f big -Wc \$(date +%T); \$*"
 alias ls='ls -FC --color=auto'
 export VISUAL=vim
 export EDITOR=vim
+
+[[ -f /usr/local/Bluemix/bx/zsh_autocomplete ]] && source /usr/local/Bluemix/bx/zsh_autocomplete
+[[ -f ~/.ssh/bluemix.json ]] &&  export BLUEMIX_API_KEY=$(grep apiKey ~/.ssh/bluemix.json | awk -F\" '{print $4}')
+
+#[[ -f /etc/profile.d/npm.sh ]] && . /etc/profile.d/npm.sh
+
--- .zshrc
+++ .zshrc
@@ -58,11 +58,3 @@ source $ZSH/oh-my-zsh.sh
 
 # User configuration
 
-export EDITOR='vim'
-export VISUAL='vim'
-
-#Bluemix config
-[[ -f /usr/local/Bluemix/bx/zsh_autocomplete ]] && source /usr/local/Bluemix/bx/zsh_autocomplete
-[[ -f ~/.ssh/bluemix.json ]] &&  export BLUEMIX_API_KEY=$(grep apiKey ~/.ssh/bluemix.json | awk -F\" '{print $4}')
-
-
